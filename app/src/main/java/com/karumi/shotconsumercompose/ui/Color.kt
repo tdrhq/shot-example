2d370456ecb3b86e2f61c9754f30379438d90a76
Arnold Noronha <arnold@tdrhq.com>
2022-09-21 19:16:28 -0400

    ..

--- app/src/main/java/com/karumi/shotconsumercompose/ui/Color.kt
+++ app/src/main/java/com/karumi/shotconsumercompose/ui/Color.kt
@@ -2,7 +2,7 @@ package com.karumi.shotconsumercompose.ui
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(xFF0000000)
+val purple200 = Color(0xFF000000)
 val purple500 = Color(0xFF6200EE)
 val purple700 = Color(0xFF3700B3)
 val teal200 = Color(0xFF03DAC5)
